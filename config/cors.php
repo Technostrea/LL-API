1a468da56b2e61424939bff061fa606d6f472456
benaja-bendo <benaja.bendo02@gmail.com>
2024-10-25 19:37:24 +0200

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:4200'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:4200','http://127.0.0.1:8000'],
 
     'allowed_origins_patterns' => [],
 
